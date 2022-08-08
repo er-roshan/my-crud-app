0d50b117b5f8c008daa39f69d9224fe66edb5d6e
er-roshan <roshan.adhikari.se@gmail.com>
2022-08-08 23:46:43 +0545

    Feat: Additional packages added;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { User } from './user/entity/user.entity';
+import { AuthModule } from './auth/auth.module';
 @Module({
   controllers: [AppController],
-  imports: [UserModule],
-
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest-crud-app',
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule { }
